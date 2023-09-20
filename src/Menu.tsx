30a2d469f7ce53f1d8d392a427495e1191023dd5
Vishnusbhat <93111324+Vishnusbhat@users.noreply.github.com>
2023-09-20 22:49:03 +0530

    Initial commit

---
+++ src/Menu.tsx
@@ -0,0 +1,14 @@
+export function Menu() {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <a href="httpss://www.google.com"> list</a>
+        </li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Search</li>
+      </ul>
+    </nav>
+  );
+}
